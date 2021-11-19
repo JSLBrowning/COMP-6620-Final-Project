136ad2424cb7c4af15dbb9a4aa935265bf38d9d0
James <james@wallofhistory.com>
2021-11-18 20:46:47 -0600

    Remove old index.php, add proper titles and favicon

--- web/register.php
+++ web/register.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/css/main.css">
+    <link rel="shortcut icon" href="/assets/bjc.png" />
 </head>
 
 <body>
